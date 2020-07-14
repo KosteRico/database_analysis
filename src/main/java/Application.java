a3e2aced78ba539c50b10d3661ad33b1183fc5b2
kosterico <kostyaparahin@gmail.com>
2020-07-14 11:07:29 +0400

    System variables settings was added

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,7 @@ import databases.MapdDatabase;
 import databases.PostgresDatabase;
 import org.apache.commons.lang3.time.StopWatch;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -18,20 +19,22 @@ public class Application {
 
     private static final Logger logger = LogManager.getLogger(Application.class);
 
+    private static String formatSqlWithTable(String sql) {
+        return String.format(sql, System.getenv("table"));
+    }
+
     private static List<String> getTestSqlQueries() {
         List<String> queries = new ArrayList<>();
 
-        queries.add("select * from logs limit 10");
-        queries.add("select * from logs limit 100");
-//        queries.add("select * from logs limit 1000");
-//        queries.add("select * from logs limit 10000");
-//        queries.add("select * from logs limit 100000");
+        queries.add(formatSqlWithTable("select * from %s limit 10"));
+        queries.add(formatSqlWithTable("select * from %s limit 100"));
+        queries.add(formatSqlWithTable("select * from %s limit 1000"));
+        queries.add(formatSqlWithTable("select * from %s limit 10000"));
+        queries.add(formatSqlWithTable("select * from %s limit 100000"));
 
-        queries.add("select count(*) from logs limit " + Integer.MAX_VALUE);
-        queries.add("select count(*), \"problem_number\" from logs group by \"problem_number\" limit " + Integer.MAX_VALUE);
-        queries.add("select avg(\"problem_number\") from logs group by \"problem_number\" limit " + Integer.MAX_VALUE);
-//        queries.add("select * from logs limit 10");
-//        queries.add("select * from logs limit 10");
+        queries.add(formatSqlWithTable("select count(*) from %s limit " + Integer.MAX_VALUE));
+        queries.add(formatSqlWithTable("select count(*), \"problem_number\" from %s group by \"problem_number\" limit " + Integer.MAX_VALUE));
+        queries.add(formatSqlWithTable("select avg(\"problem_number\") from %s group by \"problem_number\" limit " + Integer.MAX_VALUE));
 
         return queries;
     }
@@ -49,23 +52,30 @@ public class Application {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         BasicConfigurator.configure();
 
+        Logger.getRootLogger().setLevel(Level.INFO);
+
         StopWatch stopWatch = new StopWatch();
 
         List<Database> databases = getDatabases();
         List<String> sqlQueries = getTestSqlQueries();
 
-        logger.debug("Connected");
+        logger.info("Connected");
 
         PreparedStatement statement;
         FileWriter writer = new FileWriter("results.txt");
 
         for (String sql : sqlQueries) {
             writer.write(sql + " :\n\n");
+
+            logger.info(sql);
+
             for (Database database : databases) {
 
                 float timeRes = 0.0f;
 
                 final int numResults = 10;
+
+                logger.info(database.getClass() + ":");
 
                 for (int i = 0; i < numResults; i++) {
                     statement = database.getConnection().prepareStatement(sql);
@@ -77,6 +87,8 @@ public class Application {
                     timeRes += stopWatch.getTime() / 1000f;
 
                     statement.close();
+
+                    logger.info(i + 1);
 
                     stopWatch.reset();
                 }
@@ -94,5 +106,7 @@ public class Application {
         }
 
         writer.close();
+
+        logger.info("Done!!!");
     }
 }
